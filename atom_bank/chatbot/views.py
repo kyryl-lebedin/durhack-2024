027cf9bc9c834300a9f3e95f3c119599d1fa95c8
sfskldfj <sfskldfj>
2024-11-03 10:47:15 +0000

    added reply

--- atom_bank/chatbot/views.py
+++ atom_bank/chatbot/views.py
@@ -1,7 +1,10 @@
 # chatbot/views.py
-
+import sys
 from django.shortcuts import render
 from django.http import JsonResponse
+sys.path.append('../')
+import autogen_hackathon
+
 import json
 
 def chat_view(request):
@@ -10,8 +13,12 @@ def chat_view(request):
     elif request.method == 'POST':
         data = json.loads(request.body)
         user_message = data.get('message', '')
-
+        resp = autogen_hackathon.autogen_metrics(user_message)[1]
         # Placeholder response
-        response_message = f"You said: {user_message}"
+        response_message = """"""
+        for i in resp:
+            response_message += f"""- {i}\n"""
+
+        
 
         return JsonResponse({'response': response_message})
